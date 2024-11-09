6125ce46920e76eefabcdbd9eb2ddd42d9afa17f
madhu <madhumolaka018@gmail.com>
2024-11-09 08:39:50 +0000

    change code

--- app.js
+++ app.js
@@ -47,12 +47,13 @@ const accesstoken = (request, response, next) => {
   }
 }
 
-//const getfollowingpeopleIdofuser = async username => {
-//  const getfollowingpeoplequery = `SELECT following_user_id FROM follower INNER JOIN user ON user.user_id=follower.follower_user_id
-//WHERE username='${username}';`
-//const followingresult = await db.all(getfollowingpeoplequery)
-// return followingresult
-//}
+const userfollowingId = async username => {
+  const dbquery = `SELECT following_user_id FROM follower INNER JOIN user ON user.user_id=follower.follower_user_id
+  WHERE user.username='${username}';`
+  const dbresult = await db.all(dbquery)
+  const arrayId = dbresult.map(eachid => eachid.following_user_id)
+  return arrayId
+}
 
 //Api 1
 
@@ -62,62 +63,79 @@ app.post('/register/', async (request, response) => {
   //console.log(hashedPassword)
   const getdbdetails = `SELECT * FROM user WHERE username='${username}';`
   const userdbresult = await db.get(getdbdetails)
-  if (userdbresult === undefined) {
+  if (userdbresult !== undefined) {
+    response.status(400)
+    response.send('User already exists')
+  } else {
     if (password.length < 6) {
+      response.status(400)
+      response.send('Password is too short')
+    } else {
       const createuserquery = `INSERT INTO user(username,password,name,gender)
-            VALUES('${username}','{hashedPassword}','${name}','${gender}');`
+                 VALUES('${username}','{hashedPassword}','${name}','${gender}');`
       await db.run(createuserquery)
       response.status(200)
-      response.send('user created successfully')
-    } else {
-      response.send(400)
-      response.send('Password is too short')
+      response.send('User created successfully')
     }
-  } else {
-    response.status(400)
-    response.send('user already exists')
   }
 })
 
 //api 2
 
-app.post('/login/', accesstoken, async (request, response) => {
+app.post('/login/', async (request, response) => {
   const {username, password} = request.body
   //console.log(username)
 
   const getuserdbdetails = `SELECT * FROM user WHERE username='${username}';`
   const userdbresult = await db.get(getuserdbdetails)
   //console.log(userdbresult)
-  if (userdbresult === undefined) {
-    response.status(400)
-    response.send('Invalid user')
-  } else {
+  if (userdbresult !== undefined) {
     const ispasswordmatched = await bcrypt.compare(
       password,
       userdbresult.password,
     )
-    //console.log(ispasswordmatched)
-    if (ispasswordmatched === true) {
+
+    if (ispasswordmatched) {
       const payload = {username: username, userId: userdbresult.user_id}
       const jwttoken = jwt.sign(payload, 'NEW_ACCESS')
 
       response.send({jwttoken})
     } else {
       response.status(400)
-      response.send('Invalid Password')
+      response.send('Invalid password')
     }
+  } else {
+    response.status(400)
+    response.send('Invalid user')
   }
 })
 
 //api 3
-//app.get('/user/tweets/feed/', authenticatetoken, async (request, response) => {
-//const {username} = request
-//const getfollowingpeopleuserid = `SELECT following_user_id FROM follower INNER JOIN user ON user.user_id=follower.follower_user_id
-// WHERE username='${username}';`
-// const result = await db.run(getfollowingpeopleuserid)
-//const gettweetquery = `SELECT username,tweet,date_time as dateTime FROM user INNER JOIN tweet ON user.user_id=tweet.user_id
-//WHERE user.user_id='${getfollowingpeopleuserid}'ORDER BY date_time DESC LIMIT 4;
-//`
-//const tweetresult = await db.all(getfollowingpeopleuserid)
-//response.send(tweetresult)
-//})
+app.get('/user/tweets/feed/', accesstoken, async (request, response) => {
+  const {username} = request
+  const followingpeopleIds = await userfollowingId(username)
+  const dbquery = `SELECT username,tweet,date_time as dateTime FROM user INNER JOIN tweet ON user.user_id=tweet.user_id
+  WHERE user.user_id='${followingpeopleIds}'ORDER BY date_time DESC LIMIT 4;`
+  const tweets = await db.all(dbquery)
+  response.send(tweets)
+})
+
+//api 4
+app.get('/user/following/', accesstoken, async (request, response) => {
+  const {username, userId} = request
+  const dbquery = `SELECT name FROM follower INNER JOIN user ON user.user_id=follower.following_user_id
+  WHERE follower_user_id='${userId}';`
+  const result = await db.all(dbquery)
+  response.send(result)
+})
+
+//api 5
+app.get('/user/followers/', accesstoken, async (request, response) => {
+  const {username, userId} = request
+  const dbquery = `SELECT DISTINCT name FROM follower INNER JOIN user ON user.user_id=follower.follower_user_id
+ WHERE following_user_id='${userId}';`
+  const result = await db.all(dbquery)
+  response.send(result)
+})
+
+module.exports = app
